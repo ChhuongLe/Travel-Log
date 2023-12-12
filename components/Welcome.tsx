d97900bccbfc75bf4ab7e422d596c1ddfbd08a2a
Chhuong Le <chhuongle@gmail.com>
2023-12-12 12:23:42 -0800

    add sample welcome image

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Welcome () {
   return (
-    <div>
-      Welcome Section
+    <div className="flex justify-center items-center">
+      <Image src="https://i.imgur.com/QUk6w0H.jpg" alt="" width={1000} height={1000} className="w-auto h-screen py-10"/>
     </div>
   )
 }
