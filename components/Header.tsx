be54741153fe2471e6017c60dfea9500bc9a8cec
Chhuong Le <chhuongle@gmail.com>
2023-12-12 11:31:22 -0800

    add theme toggler and initialize firebase app

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,17 +2,18 @@ import { SignIn, SignInButton, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from 'next/image'
 import Logo from '@/public/Logo.png'
 import Link from 'next/link'
+import { ThemeToggler } from "./ThemeToggler"
 
 export default function Header () {
   return (
     <header className="flex items-center justify-between">
       <div>
         <Link href='/' className="flex">
-          <Image src= {Logo} alt="" width={150} height={150}/>
+          <Image src= {Logo} alt="" className="" width={150} height={150}/>
         </Link>
       </div>
-      <div className="flex px-5">
-      {/* Theme Toggler */}
+      <div className="flex items-center space-x-2 px-5">
+      <ThemeToggler />
       <UserButton afterSignOutUrl="/"/>
       <SignedOut>
         <SignInButton afterSignInUrl="/" mode="modal" />
---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDR9wQTgjRspbT6bZ04DVTsl-vA15HSqs",
+  authDomain: "travel-log-789cb.firebaseapp.com",
+  projectId: "travel-log-789cb",
+  storageBucket: "travel-log-789cb.appspot.com",
+  messagingSenderId: "1068981928652",
+  appId: "1:1068981928652:web:b0911367468e2433cdf869"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
