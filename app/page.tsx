486d8ae2ab23248a764b337da2d25fe2466a97f1
Chhuong Le <chhuongle@gmail.com>
2023-12-12 12:06:06 -0800

    complete header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import Welcome from '@/components/Welcome'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="">
-      Main Page
+      <section id="welcome">
+        <Welcome />
+      </section>
+      <section id="categories">
+        {/* Categories */}
+      </section>
+      <section id="about">
+        {/* About */}
+      </section>
+      <section id="contact">
+        {/* Contact */}
+      </section>
     </main>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,11 +6,19 @@ import { ThemeToggler } from "./ThemeToggler"
 
 export default function Header () {
   return (
-    <header className="flex items-center justify-between">
+    <header className="flex items-center justify-between border-b shadow-lg">
       <div>
         <Link href='/' className="flex">
           <Image src= {Logo} alt="" className="" width={150} height={150}/>
         </Link>
+      </div>
+      <div className="flex flex-row space-x-5">
+        <Link href="#welcome">
+          <p>Welcome</p>
+        </Link>
+        <p>Categories</p>
+        <p>About</p>
+        <p>Contact</p>
       </div>
       <div className="flex items-center space-x-2 px-5">
       <ThemeToggler />
